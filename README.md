b87284c41b00f0bf016ec386d9d22441a8b19715
accelerator-01 <acceleratorgame01@gmail.com>
2023-11-14 21:36:04 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Creating Professional Real world project using:
+
+### React Router
+
+### Redux
+
+### Tailwind
---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import AppLayout from "./ui/AppLayout";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import Order from "./features/order/Order";
+import CreateOrder from "./features/order/CreateOrder";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
