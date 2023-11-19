53f91a30a9e23dfa385a61207a51540999169ea9
accelerator-01 <acceleratorgame01@gmail.com>
2023-11-19 21:48:58 +0700

    Finishing the styles of the project

--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -7,12 +7,12 @@ function OrderItem({ item, isLoadingIngredients, ingredients }) {
   console.log(ingredients);
 
   return (
-    <li>
-      <div>
+    <li className="py-3">
+      <div className="flex items-center justify-between text-sm">
         <p>
-          <span>{quantity}&times;</span> {name}
+          <span className="font-bold">{quantity}&times;</span> {name}
         </p>
-        <p>{formatCurrency(totalPrice)}</p>
+        <p className="font-bold">{formatCurrency(totalPrice)}</p>
       </div>
     </li>
   );
