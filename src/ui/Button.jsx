14543c5563d4999fd261d137164d68870aefd3d8
accelerator-01 <acceleratorgame01@gmail.com>
2023-11-19 13:59:58 +0700

    styling button in cart page

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -7,6 +7,8 @@ function Button({ children, disabled, to, type }) {
   const styles = {
     primary: base + " px-4 py-3 sm:px-6 sm:py-4",
     small: base + "  px-4 py-2 md:px-5 md:py-2.5",
+    secondary:
+      "px-4 py-2.5 sm:px-6 sm:py-3.5 inline-block rounded-full border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed",
   };
   if (to)
     return (
