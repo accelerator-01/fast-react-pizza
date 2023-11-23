f85f7e9645d38f5017f705eb2e39d7258fc423fd
accelerator-01 <acceleratorgame01@gmail.com>
2023-11-23 21:05:27 +0700

    just updating cart, today watching Bungou Stray Dogs. It is so fun hehehe

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,11 +1,24 @@
 import { formatCurrency } from "../../utils/helpers";
+import { useDispatch } from "react-redux";
 import PropTypes from "prop-types";
 import Button from "../../ui/Button";
+import { addItem } from "../cart/cartslice";
 
 function MenuItem({ pizza }) {
-  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
-  console.log(id);
+  const dispatch = useDispatch();
 
+  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  function handleAddToCart() {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+    dispatch(addItem(newItem));
+  }
   return (
     <li className="flex gap-4 py-2">
       <img
@@ -27,7 +40,11 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          <Button type="small">Add to Cart</Button>
+          {!soldOut && (
+            <Button type="small" onClick={handleAddToCart}>
+              Add to Cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
